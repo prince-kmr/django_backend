6df9f3ee8c5e93bd482969d0848103a9841857ee
prince-kmr <princekmr9852@gmail.com>
2026-06-08 23:44:55 +0530

    Dynamic routing added

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello world")
+# def home(request):
+#     return HttpResponse("Hello world")
+
+def courseDetails(request, courseid):
+    return HttpResponse(courseid)
--- letsdo/urls.py
+++ letsdo/urls.py
@@ -20,5 +20,5 @@ from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home)
+    path('course/<int:courseid>', views.courseDetails)
 ]
